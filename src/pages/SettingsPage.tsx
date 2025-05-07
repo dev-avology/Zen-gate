72d41dbe09a7c965cda003ab7ed2239c6c5be1e0
dev-avology <devavology12@gmail.com>
2025-05-07 13:26:16 +0530

    added

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -20,7 +20,7 @@ async function getUserData() {
     });
 
      // Send encrypted data to your backend for decryption
-     const response = await fetch('hhttps://phpstack-1180784-5431682.cloudwaysapps.com/api/decrypt-data', {
+     const response = await fetch('https://phpstack-1180784-5431682.cloudwaysapps.com/api/decrypt-data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
