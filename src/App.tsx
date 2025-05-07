c33fd3a774ec506a86cf44cd32e05d0a77c694e2
dev-avology <devavology12@gmail.com>
2025-05-07 15:11:10 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import { Toaster } from 'react-hot-toast';
 const App: React.FC = () => {
   return (
     <Router>
+      <Toaster position="top-right" reverseOrder={false} />
       <Routes>
-        <Toaster position="top-right" reverseOrder={false} />
         <Route path="/" element={<CheckoutPage />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/oauth-redirect" element={<OAuthRedirect />} />
