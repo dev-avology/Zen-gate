8238b2b539b4397bc5e05b84dd0a47cc981317af
dev-avology <devavology12@gmail.com>
2025-05-08 11:51:37 +0530

    added

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -97,17 +97,10 @@ export default function CheckoutForm() {
 
   return (
     <div>
-      {/* <h2>Checkout</h2> */}
-      {/* <div id="my-div" /> */}
-      {/* <button onClick={handleSubmit}>Pay</button> */}
-      {/* {error && <p style={{ color: 'red' }}>{error}</p>} */}
-
-      <iframe
-        src="https://phpstack-1180784-5431682.cloudwaysapps.com/webhook/payment-page"
-        title="Payment Page"
-        style={{ width: '100%', height: '100%', border: 'none' }}
-        allowFullScreen
-      ></iframe>
+      <h2>Checkout</h2>
+      <div id="my-div" />
+      <button onClick={handleSubmit}>Pay</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
   );
 }
