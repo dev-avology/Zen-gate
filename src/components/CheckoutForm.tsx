b6f99344de1d3185063fad28b0731453b30f4c70
dev-avology <devavology12@gmail.com>
2025-05-08 13:07:29 +0530

    added

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -33,7 +33,7 @@ export default function CheckoutIframePage() {
           scriptLoaded.current = true;
         }
 
-        const tokenizationSourceKey = 'pk_abc123'; // Replace with your actual public key
+        const tokenizationSourceKey = '0Xe9TlQpkpfQf6ZFxT2ozBqS5Z5Ey7sD'; // Replace with your actual public key
         const options = { target: '#card-form' };
 
         if (!tokenizationRef.current && window.HostedTokenization) {
